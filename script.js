a3ec0b112eefa828a41f101afb053304ca7bc0f4
Victor-Aguiar <victorgoncalvesaguiar@gmail.com>
2026-06-08 21:10:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let horasInput = Document.querySelector("#horas")
+let minutosInput = document.querySelector("#minutos")
+let segundosInput = document.querySelector("#segundos")
+
+
+
+setInterval(() => {
+    let date = new Date();
+    let horas = date.getHours();
+    let minutos = date.getMinutes();
+    let segundos = date.getSeconds();
+
+    horasInput.innerHTML = horas;
+    minutosInput.innerHTML = minutos;
+    segundosInput.innerHTML = segundos;
+
+    if(segundos < 10) {
+        segundosInput.innerHTML = `0${segundos}`;
+    }
+}, 1000);
